fb48d940479c1c9ffb9e0017724a89ba67dafaed
Avinash Singh <avinash.n.singh@accenture.com>
2018-11-12 16:48:34 +0530

    change in git-service

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -5,6 +5,19 @@ import { GitSearch } from './git-search';
 export class GitSearchService {
   //set up a cache for our service the first time it is injected.
   cachedValues: Array<{ [query: string]: GitSearch }> = [];
+
   constructor() { }
 
+  gitSearch = (query:string)=>{
+    let promise = new Promise((resolve,reject)=>{
+      if(this.cachedValues[query]){
+        resolve(this.cachedValues[query]);
+      }
+      else{
+        
+      }
+    });
+
+    return promise;
+  }
 }
